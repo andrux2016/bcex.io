a57bef35560a34f76f3adf0f01359e938cd246aa
ftrentini <ftrentini@gmx.us>
2017-09-22 00:28:37 +0000

    Adding BRL to Peatio

--- app/controllers/private/assets_controller.rb
+++ app/controllers/private/assets_controller.rb
@@ -3,13 +3,13 @@ module Private
     skip_before_action :auth_member!, only: [:index]
 
     def index
-      @cny_assets  = Currency.assets('cny')
+      @brl_assets  = Currency.assets('brl')
       @btc_proof   = Proof.current :btc
-      @cny_proof   = Proof.current :cny
+      @brl_proof   = Proof.current :brl
 
       if current_user
         @btc_account = current_user.accounts.with_currency(:btc).first
-        @cny_account = current_user.accounts.with_currency(:cny).first
+        @brl_account = current_user.accounts.with_currency(:brl).first
       end
     end
 
