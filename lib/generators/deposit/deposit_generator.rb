9c1b10a2bcd10913b744487f66215193e3152cf2
ftrentini <ftrentini@gmx.us>
2017-09-22 03:52:58 +0000

    Setting BCEX TRADER

--- lib/generators/deposit/deposit_generator.rb
+++ lib/generators/deposit/deposit_generator.rb
@@ -6,7 +6,7 @@ class DepositGenerator < Rails::Generators::NamedBase
   def copy_initializer_file
     template "model.rb.erb", "app/models/deposits/#{name.underscore}.rb"
     template "controller.rb.erb", "app/controllers/private/deposits/#{name.underscore.pluralize}_controller.rb"
-    template "locales/zh-CN.yml.erb", "config/locales/deposits/#{name.underscore.pluralize}/zh-CN.yml"
+    template "locales/pt-BR.yml.erb", "config/locales/deposits/#{name.underscore.pluralize}/pt-BR.yml"
     template "locales/en.yml.erb", "config/locales/deposits/#{name.underscore.pluralize}/en.yml"
     template "views/new.html.slim.erb", "app/views/private/deposits/#{name.underscore.pluralize}/new.slim"
   end
